947d4bfca6cd57f7de6b8c4de7533c1296f966d6
S <thavamani.raja@hpe.com>
2020-05-31 23:32:13 +0530

    removing cpu and memroy variables

--- vsphere-standalone.tf
+++ vsphere-standalone.tf
@@ -59,9 +59,7 @@ resource "vsphere_virtual_machine" "standalone" {
 
   disk {
     label            = "${var.vm_name}.vmdk"
-    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
-    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
-    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+    size             = 60
   }
 
   clone {
